64ecc4e851ded57c2d293987f207168f06da0524
Ben <69478311+mojo626@users.noreply.github.com>
2024-04-26 19:44:33 -0700

    boat turning

--- src/util/Player.hpp
+++ src/util/Player.hpp
@@ -19,6 +19,13 @@ class Player {
         int SCREEN_WIDTH;
         int SCREEN_HEIGHT;
 
+        glm::vec2 move;
+        double curRot;
+
+        glm::vec2 pos;
+        double speed;
+        double waterForce = 0.9;
+
         Player(Camera* cam, int screenWidth, int screenHeight)
         {
             SCREEN_HEIGHT = screenHeight;
@@ -32,35 +39,63 @@ class Player {
             renderer = SpriteRenderer(shader, "../res/images/Boat.png");
         }
 
+        void ProcessInputs(Inputs input)
+        {
+            
+        }
+
         void Render(Camera* cam, Inputs input)
         {
             float speed = 1;
-            glm::vec2 move;
+
             if (input.DOWN)
             {
-                move.y -= speed;
+                move.y = -speed;
+                if(!input.LEFT && !input.RIGHT)
+                {
+                    move.x = 0;
+                }
             } 
             if (input.LEFT)
             {
-                move.x -= speed;
+                move.x = -speed;
+                if(!input.UP && !input.DOWN)
+                {
+                    move.y = 0;
+                }
             } 
             if (input.RIGHT)
             {
-                move.x += speed;
+                move.x = speed;
+                if(!input.UP && !input.DOWN)
+                {
+                    move.y = 0;
+                }
             }
             if (input.UP)
             {
-                move.y += speed;
+                move.y = speed;
+                if(!input.LEFT && !input.RIGHT)
+                {
+                    move.x = 0;
+                }
             }
 
             float rot = atan2(move.x, move.y);
+
+            if (rot < curRot) {
+                curRot -= 0.1;
+            }
+            if (rot > curRot) {
+                curRot += 0.1;
+            }
             
 
             shader.use();
 
             shader.SetMatrix4("projection", cam->GetProjection());
 
-            renderer.DrawSprite(glm::vec2(cam->pos.x + SCREEN_WIDTH/4-10, cam->pos.y + SCREEN_HEIGHT/4-10), glm::vec2(20, 20), rot/3.14*180 , glm::vec3(1.0, 1.0, 1.0));
+            renderer.DrawSprite(glm::vec2(cam->pos.x + SCREEN_WIDTH/4-10, cam->pos.y + SCREEN_HEIGHT/4-10), glm::vec2(20, 20), curRot/3.14*180 , glm::vec3(1.0, 1.0, 1.0));
         }
 };
 
